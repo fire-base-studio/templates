3c79ca83be68994519c599b6d53616339f2774a4
noshimorimoshi <noshimorimoshi@gmail.com>
2025-10-01 08:03:44 +0000

    feat: add svelte-langchain template

---
+++ svelte-langchain/idx-template.nix
@@ -0,0 +1,24 @@
+{ pkgs, language ? "js", ... }: {
+  packages = [
+    pkgs.nodejs_20
+  ];
+  bootstrap = ''
+    mkdir -p "$WS_NAME"
+    npm create -y vite@latest "$WS_NAME" -- --template ${if language == "ts" then "svelte-ts" else "svelte"}
+    mkdir -p "$WS_NAME/.idx/"
+    cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
+    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
+    chmod -R +w "$WS_NAME"
+    mv "$WS_NAME" "$out"
+
+    mkdir -p "$out/.idx"
+    chmod -R u+w "$out"
+    cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
+    cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
+    chmod -R u+w "$out"
+    
+    cd "$out"
+    npm install --package-lock-only --ignore-scripts
+    npm install --ignore-scripts @google/generative-ai @langchain/core @langchain/google-genai base64-js markdown-it
+  '';
+}
